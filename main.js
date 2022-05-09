6d7e13440dd9264404fc34cd017191811e8af3ac
TvdH-UX <thomasheuvel7@gmail.com>
2022-05-09 12:33:29 +0200

    Add operate function

--- main.js
+++ main.js
@@ -15,3 +15,13 @@ const multiply = function(number1, number2){
 const divide = function(number1, number2){
     return number1 / number2;
 };
+
+// Operate function
+// Take operator and a first and second number
+// Call on provided operator
+// Insert the two numbers in order
+// Return the outcome
+
+const operate = function(operator, number1, number2){
+    return operator(number1, number2);
+};
