8c44589bbe978fbdd8b28d58579d104c5c82d37f
TvdH-UX <thomasheuvel7@gmail.com>
2022-05-18 12:53:01 +0200

    Improve division by 0 handling

--- main.js
+++ main.js
@@ -21,10 +21,10 @@ const multiply = function(number1, number2){
 };
 
 const divide = function(number1, number2){
-    // if(Infinity){
-    //     alert("Ah ah ah, you better start over");
-    //     return display.textContent = "Fatal error, please restart";
-    // }
+    if((number1 / number2) == Infinity || (number1 / number2) == -Infinity){
+        alert(`No division by 0 please!`);
+        return NaN;
+    }
     return number1 / number2;
 };
 
@@ -41,30 +41,12 @@ const operate = function(operator, number1, number2){
     };
 };
 
-const reset = function(){
-    alert('hoi')
-}
-
-// ALGORITHM
-// Initialize number buttons for user to click
-// Initialize operator buttons for user to click
-// Initialize clear button for user to start fresh
-// When a user inputs a number
-// Display number on calculator display
-// Save the value on the display
-
-// Initialize variables to save values in to calculate with
 let previousNumber = '';
 let currentNumber = '';
 let currentOperator = '';
 let result = '';
 let previousFunctionBtn = '';
 
-// Initialize buttons function
-// First load in all the buttons that contain a number
-// Make each number button clickable
-// When a user clicks a number
-// Update the display value to the entered number
 numbers.forEach((number) => {
     number.addEventListener('click', () => {
         handleNumber(number.value);
@@ -76,7 +58,6 @@ const handleNumber = function(number) {
     display.textContent = currentNumber;
 };
 
-// Entering an operator
 operators.forEach((operator) => {
     operator.addEventListener('click', () => {
         handleOperator(operator.value);
@@ -90,20 +71,13 @@ const handleOperator = function(operator) {
         display.textContent = currentOperator;
         currentNumber = '';
         previousFunctionBtn = operator;
-        // console.log(previousFunctionBtn);
     } else if (previousFunctionBtn == '+' || previousFunctionBtn == '-' || previousFunctionBtn == '×' || previousFunctionBtn == '÷') {
-        // console.log("we're in");
-        // console.log("previous number: " +  previousNumber);
-        // console.log("current number: " +  currentNumber);
-        // console.log("current operator: " + currentOperator);
         handleEquals();
         currentOperator = operator;
         previousNumber = currentNumber;
         display.textContent = currentOperator;
         currentNumber = '';
         previousFunctionBtn = operator;
-        // console.log(previousFunctionBtn);
-        // console.log("new operator: " + currentOperator);
     };
 };
 
@@ -117,10 +91,7 @@ const handleEquals = function(){
         displayResult.textContent = +result.toFixed(2);
         display.textContent = "";
         currentNumber = result;
-        // console.log(previousNumber);
-        // console.log(currentNumber);
         previousFunctionBtn = '=';
-        // console.log(previousFunctionBtn);
     };
 };
 
@@ -137,14 +108,3 @@ const handleClear = function(){
     displayResult.textContent = '';
     display.textContent = 0;
 };
-
-// If current result same as previous result
-// If last input is result
-// If a second operator variable is filled, first do calculation
-// Reset the second operator variable to contain nothing
-// Check against current result
-
-// The moment another operator is pressed
-// Check something
-// Calculate result
-// Continue inputs
